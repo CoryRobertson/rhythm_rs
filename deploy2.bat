22246fd317cd1a8c9c917c6382bfd8589ef36c37
coryrobertson <cory.robertson17@gmail.com>
2022-10-21 23:13:21 -0700

    Updated deploy script template

--- deploy2.bat
+++ deploy2.bat
@@ -2,6 +2,7 @@
 trunk build --release
 docker build -t rhythm_rs .
 docker save -o IMAGELOCATION rhythm_rs:latest
-scp LOCATION DESTINATION
-ssh SERVER docker load -i DESTINATION ON SERVER
-ssh SERVER DEPLOYMENT DOCKER COMMAND
+ssh SERVER "cd LOCATIONOFCONTAINER ; docker-compose down ; docker image rm rhythm_rs"
+scp IMAGELOCATION SERVER:LOCATIONOFCONTAINER
+ssh SERVER docker load -i LOCATIONOFCONTAINER
+ssh SERVER "cd LOCATIONOFCONTAINER ; docker-compose up -d"
